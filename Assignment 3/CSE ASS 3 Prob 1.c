ad240a1331156c7ac2def6e056aab28a4a7f6c9b
Busetty Charan <94380841+Busetty-Charan@users.noreply.github.com>
2021-11-29 16:06:50 +0000

    Assignment 3 problem 1

---
+++ Assignment 3/CSE ASS 3 Prob 1.c
@@ -0,0 +1,22 @@
+// Program to print a pascal triangle
+
+// Include required headers
+#include <stdio.h>
+
+// Start program from here
+int main()
+{
+    // Print the pascal triangle
+    for(int i = 1; i <= 4; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+
+        printf("\n");
+    }
+
+    // Return zero to show success
+    return 0;
+}
