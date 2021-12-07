57335621d0c616ae7aa1c1f362ff9412a7f6062e
Busetty-Charan <125012012@sastra.ac.in>
2021-12-07 11:56:34 +0530

    completed 4th sum

---
+++ Assignment 4/CSE ASS 4 Prob 4.c
@@ -0,0 +1,47 @@
+// Program to calcuate car price in the showroom
+
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+    int next;
+
+    do
+    {
+        int car_price, road_tax, discount, gst;
+
+        printf("Enter the price of the car : ");
+        scanf("%d", &car_price);
+
+        printf("Enter the amount of road tax : ");
+        scanf("%d", &road_tax);
+
+        printf("Enter the disount percentage : ");
+        scanf("%d", &discount);
+
+        if (car_price < 500000)
+        {
+            gst = 8 * (car_price / 100);
+        }
+        else if (car_price >= 500000 && car_price < 1000000)
+        {
+            gst = 18 * (car_price / 100);
+        }
+        else
+        {
+            gst = 28 * (car_price / 100);
+        }
+
+        printf("The on-road price of this car is : %d\n", (car_price + road_tax + gst) - (discount * (car_price / 100)));
+        
+        printf("Enter '1' if you want to continue : ");
+        scanf("%d", &next);
+
+        if (next != 1)
+        {
+            next = 0;
+        }
+    }while (next);
+    
+    return 0;
+}
