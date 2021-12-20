7433d89e3453a07eeca6205389565c57fa09d129
Busetty-Charan <125012012@sastra.ac.in>
2021-12-20 18:47:22 +0530

    Completed binary search using recursion

---
+++ Assignment 6/CSE ASS 6 Prob 1.c
@@ -0,0 +1,60 @@
+// Program to find an element in an array using Binary Search using recursion
+
+#include <stdio.h>
+
+int binary_search (int bins_arr[], int first, int last, int checker)
+{
+    if (last >= first)
+    {
+        int middle;
+
+        middle = first + (last - first) / 2;
+
+        if (bins_arr[middle] == checker)
+        {
+            return middle;
+        }
+        else if (bins_arr[middle] > checker)
+        {
+            return binary_search(bins_arr, first, middle - 1, checker);
+        }
+        else
+        {
+            return binary_search(bins_arr, middle + 1, last, checker);
+        }
+    }
+
+    return -1;
+}
+
+int main (int argc, char *argv[])
+{
+    int n, checker, bins_result;
+
+    printf("How many elements you would like to pass : ");
+    scanf(" %d", &n);
+
+    int bins_arr[n - 1];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d : ", i + 1);
+        scanf(" %d", bins_arr + i);
+    }
+    
+    printf("Enter what element you would like to check for : ");
+    scanf(" %d", &checker);
+
+    bins_result = binary_search(bins_arr, 0, n, checker);
+
+    if (bins_result == -1)
+    {
+        printf("The entered number was not present in the given array\n");
+    }
+    else
+    {
+        printf("The entered numer was found at index : %d\n", bins_result);
+    }
+
+    return 0;
+}
