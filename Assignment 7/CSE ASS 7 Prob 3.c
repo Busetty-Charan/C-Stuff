3809e89b9e1c9e596c921b5ad4b05a24d7229423
Busetty-Charan <125012012@sastra.ac.in>
2021-12-27 17:27:54 +0530

    done with sum 3

---
+++ Assignment 7/CSE ASS 7 Prob 3.c
@@ -0,0 +1,40 @@
+// Program to delete a value from an array based on value
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+
+    int n;
+
+    printf("How many elements would you like to have in your array : ");
+    scanf(" %d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf(" %d", arr + i);
+    }
+
+    int rem;
+    int x = 1;
+
+    printf("What element would you like to remove : ");
+    scanf(" %d", &rem);
+    printf("[ ");
+
+    for (int i = 0; i < n; i++)
+    {
+        if (*(arr + i) == rem && x != 0)
+        {
+            x = 0;
+            continue;
+        }
+
+        printf("%d, ", *(arr + i));
+    }
+
+    printf("\b\b ]\n");
+
+    return 0;
+}
